d482abeb23b5c35a5d2455c4ec65329d2238766f
Zhitao Fan <fan.zhitao.cn@gmail.com>
2015-06-20 11:10:41 -0400

    First commit. Dummy proxy, which can now just receive Internet Explore data packages. Many bugs.

---
+++ Win32ConsoleWinsockProxy/proxy.cpp
@@ -0,0 +1,153 @@
+﻿#undef UNICODE
+
+#define WIN32_LEAN_AND_MEAN
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+// calling conventions
+// https://msdn.microsoft.com/en-us/library/zkwh89ks.aspx
+
+using namespace std;
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "80"
+
+int __cdecl main(void)
+{
+	WSADATA wsaData;
+	int iResult;
+
+	SOCKET ListenSocket = INVALID_SOCKET;
+	SOCKET ClientSocket = INVALID_SOCKET;
+
+	struct addrinfo *result = NULL;
+	struct addrinfo hints;
+
+	int iSendResult;
+
+	char recvbuf[DEFAULT_BUFLEN];
+	int recvbuflen = DEFAULT_BUFLEN;
+
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0)
+	{
+		printf("WSAStartup failed with error: %d\n", iResult);
+		return 1;
+	}
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+
+	// Resolve the server address and port
+	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+	if (iResult != 0)
+	{
+		printf("getaddrinfo failed with error: %d\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+
+	// Create a SOCKET for connecting to server
+	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	if (ListenSocket == INVALID_SOCKET)
+	{
+		printf("socket failed with error: %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
+
+	printf("Proxy is on.\n");
+	
+	// Setup the TCP listening socket
+	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+	if (iResult == SOCKET_ERROR)
+	{
+		printf("bind failed with error: %d\n", WSAGetLastError());
+		freeaddrinfo(result);
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+	
+	freeaddrinfo(result);
+	
+	iResult = listen(ListenSocket, SOMAXCONN);
+	if (iResult == SOCKET_ERROR)
+	{
+		printf("listen failed with error: %d\n", WSAGetLastError());
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+	
+	// Receive until the peer shuts down the connection
+	while (1)
+	{
+		sockaddr_in sinRemote;
+		int nAddrSize = sizeof(sinRemote);
+		
+		// Accept a client socket
+		ClientSocket = accept(ListenSocket, (sockaddr*)&sinRemote, &nAddrSize);
+		if (ClientSocket == INVALID_SOCKET)
+		{
+			printf("accept failed with error: %d\n", WSAGetLastError());
+			closesocket(ListenSocket);
+			WSACleanup();
+		}
+		
+		iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+		if (iResult > 0)
+		{
+			printf("Bytes received: %d\n", iResult);
+			
+			// Echo the buffer back to the sender
+			// iSendResult = send(ClientSocket, recvbuf, iResult, 0);
+			char *test = "<html><head></head><body>Hello, Ge Wen.</body></html>"; // not working
+			iSendResult = send(ClientSocket, test, strlen(test), 0);
+			if (iSendResult == SOCKET_ERROR)
+			{
+				printf("send failed with error: %d\n", WSAGetLastError());
+				closesocket(ClientSocket);
+				WSACleanup();
+			}
+			cout << "Accepted connection from " <<
+				inet_ntoa(sinRemote.sin_addr) << ":" <<
+				ntohs(sinRemote.sin_port) << "." <<
+				endl;
+			
+			printf("Bytes sent: %d\n", iSendResult);
+			
+			// TODO: start proxy
+			// CreateThread()
+		}
+		else if (iResult == 0)
+			printf("Connection closing...\n");
+		else
+		{
+			printf("recv failed with error: %d\n", WSAGetLastError());
+			closesocket(ClientSocket);
+			WSACleanup();
+		}
+	}
+	
+	// cleanup
+	closesocket(ClientSocket);
+	WSACleanup();
+	
+	return 0;
+}
